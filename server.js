f80c7a8a1cad3b46fc0b74762a4eee0c0566393f
Tyler Wagner <t_wagner_27@hotmail.com>
2022-11-17 13:27:25 -0500

    attempt db connection

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 
 process.on('uncaughtException', (err) => {
   console.log('UHANDLED EXCEPTION! Shutting down...');
@@ -7,10 +7,10 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE;
+const DB = process.env.MONGODB_URI;
 
 mongoose
   .connect(DB, {
