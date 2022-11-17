00342c63c97ce74313ee6a8191ffdb414e5ba3c4
Tyler Wagner <t_wagner_27@hotmail.com>
2022-11-17 18:19:34 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-
-console.log(process.env);
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
-  console.log('UHANDLED EXCEPTION! Shutting down...');
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
